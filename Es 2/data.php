188de2afc9b7200b60cbc8388fff9b6ce9e1a01d
Gabriele <gabrielepese@gmail.com>
2020-09-07 17:05:59 +0200

    problem es 2 handlebars

--- Es 2/data.php
+++ Es 2/data.php
@@ -1,33 +1,81 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+<?php
+ header('Content-type: application/json');
+$data =[
+    "success" => true,
+    "response" => [
+        [
+            "poster" => "https://www.onstageweb.com/wp-content/uploads/2018/09/bon-jovi-new-jersey.jpg",
+            "title" => "New Jersey",
+            "author" => "Bon Jovi",
+            "genre" => "Rock",
+            "year" => "1988"
+        ],
+        [
+            "poster" => "https://images.pyramidshop.com/images/_popup/ACPPR48056.jpg",
+            "title" => "Live at Wembley 86",
+            "author" => "Queen",
+            "genre" => "Pop",
+            "year" => "1992"
+        ],
+        [
+            "poster" => "https://images-na.ssl-images-amazon.com/images/I/41JD3CW65HL.jpg",
+            "title" => "Ten's Summoner's Tales",
+            "author" => "Sting",
+            "genre" => "Pop",
+            "year" => "1993"
+        ],
+        [
+            "poster" => "https://cdn2.jazztimes.com/2018/05/SteveGadd-800x723.jpg",
+            "title" => "Steve Gadd Band",
+            "author" => "Steve Gadd Band",
+            "genre" => "Jazz",
+            "year" => "2018"
+        ],
+        [
+            "poster" => "https://images-na.ssl-images-amazon.com/images/I/810nSIQOLiL._SY355_.jpg",
+            "title" => "Brave new World",
+            "author" => "Iron Maiden",
+            "genre" => "Metal",
+            "year" => "2000"
+        ],
+        [
+            "poster" => "https://upload.wikimedia.org/wikipedia/en/9/97/Eric_Clapton_OMCOMR.jpg",
+            "title" => "One more car, one more raider",
+            "author" => "Eric Clapton",
+            "genre" => "Rock",
+            "year" => "2002"
+        ],
+        [
+            "poster" => "https://images-na.ssl-images-amazon.com/images/I/51rggtPgmRL.jpg",
+            "title" => "Made in Japan",
+            "author" => "Deep Purple",
+            "genre" => "Rock",
+            "year" => "1972"
+        ],
+        [
+            "poster" => "https://images-na.ssl-images-amazon.com/images/I/81r3FVfNG3L._SY355_.jpg",
+            "title" => "And Justice for All",
+            "author" => "Metallica",
+            "genre" => "Metal",
+            "year" => "1988"
+        ],
+        [
+            "poster" => "https://img.discogs.com/KOBsqQwKiNKH-q927oHMyVdDzSo=/fit-in/596x596/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-6406665-1418464475-6120.jpeg.jpg",
+            "title" => "Hard Wired",
+            "author" => "Dave Weckl",
+            "genre" => "Jazz",
+            "year" => "1994"
+        ],
+        [
+            "poster" => "https://m.media-amazon.com/images/I/71K9CbNZPsL._SS500_.jpg",
+            "title" => "Bad",
+            "author" => "Michael Jacjson",
+            "genre" => "Pop",
+            "year" => "1987"
+        ]
+    ]
+ ];
 
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/handlebars.js/4.1.2/handlebars.min.js"></script>
-    <link rel="stylesheet" href="style.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-    <script type="text/javascript" src="script.js"></script>
-</head>
-<body>
 
-    <script id="template" type="text/x-handlebars-template">
-
-        <div id="contenitore" class="contenitore">
-        <ul>
-          <li>{{{title}}}</li>
-          <li>{{author}}</li>
-          <li>{{genre}}</li>
-          <li>{{{year}}}</li>
-         
-        </div>
-        </ul>
-      </div>
-
-
-
-      </script>
-    
-</body>
-</html>
+echo json_encode($data);
+?>
