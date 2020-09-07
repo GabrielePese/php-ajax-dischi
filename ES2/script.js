60a0754fcd841b47f1a5bac06837034ae891b97d
Gabriele <gabrielepese@gmail.com>
2020-09-07 18:34:43 +0200

    FINITO

--- ES2/script.js
+++ ES2/script.js
@@ -1,23 +1,29 @@
+
 $.ajax({
     url:'data.php',
     method:'GET',
     success: function(data){
-        var data = data['response'];
+        var dataDB = data['response'];
         console.log('risultato di data[respone] = ', data);
+        getData(dataDB);
     },
     error: function(){
         console.log('error');
     }
 });
 
-var template = $("#template").html();
-var compiled = Handlebars.compile(template);
-var target = $("#contenitore");
 
-for (var i = 0; i < data.length; i++){
-    var data = data[i];
-    var dataHTML = compiled(data);
-    target.append(dataHTML);
+function getData (dataDB){
+
+    var template = $("#template").html();
+    var compiled = Handlebars.compile(template);
+    var target = $("#contenitore");
+
+
+    for (var i = 0; i < dataDB.length; i++){
+        var data = dataDB[i];
+        var dataHTML = compiled(data);
+        target.append(dataHTML);
+    }
+
 }
-
-console.log($("#template"));
